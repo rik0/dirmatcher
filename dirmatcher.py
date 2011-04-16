98a1e241b6d800aa888834bef431bd98dbb1cdb9
Enrico Franchi <efranchi@ce.unipr.it>
2011-04-16 21:40:26 +0200

    Somewhat fixed some unfinished stuff.

--- dirmatcher.py
+++ dirmatcher.py
@@ -1,19 +1,41 @@
-import haslib
+import hashlib
 import os
 from os import path
 
 
+def hash_file(make_hash, path):
+    hash = make_hash()
+    with file(path) as fh:
+        hash.update(fh.read())
+    return hash.digest()
+
+class FileGathering(object):
+    def __init__(self):
+        pass
+
+    def add(self, path, hash_value):
+        pass
+
+
 class FileGatherer(object):
     def __init__(self, root_directory,
+                 make_hash=hashlib.sha1,
                  file_filter=lambda p: True,
                  directory_filter=lambda p: True):
         self.root_directory = root_directory
         self.file_filter = file_filter
         self.directory_filter = directory_filter
+        self.make_hash = make_hash
 
     def __call__(self):
+        hashed_files = FileGathering()
         for current_root, directories, files in os.walk(root_directory):
             self.filter_directories(directories)
+            paths = self.gather_files(current_root, files)
+            for path in paths:
+                hash_value = hash_file(self.make_hash, path)
+                hashed_files.add(path, hash_value)
+        return hashed_files
 
     def filter_directories(self, directories):
         # os walk doc states that del & slicing shall be used
@@ -22,4 +44,10 @@ class FileGatherer(object):
         for index in xrange():
             directory = directories[index]
             if self.directory_filter(directory):
-                del directories[i]
+                del directories[index]
+
+    def gather_files(self, root, files):
+        return [path.join(root, file_)
+                for file_ in files if self.file_filter(file_)]
+
+
