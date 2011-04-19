e68a8d007dfab809bf90923d03e98656878cf5c8
Enrico Franchi <efranchi@ce.unipr.it>
2011-04-19 13:52:46 +0200

    Simplified implementation.

--- bimap.py
+++ bimap.py
@@ -1,48 +1,21 @@
 import collections
 
 class Bimap(collections.Sequence):
-    class PairProxy(object):
-        def __init__(self, delegate, attribute_name):
-            self.delegate = delegate
-            self.attribute_name = attribute_name
-
-        def __str__(self):
-            return str(self.delegate)
-
-        def __getattr__(self, item):
-            this = getattr(self.delegate, self.attribute_name)
-            return getattr(this, item)
-
-    class Pair(collections.namedtuple('Pair', 'left, right')):
-        def left_view(self):
-            return Bimap.PairProxy(self, 'left')
-
-        def right_view(self):
-            return Bimap.PairProxy(self, 'right')
-
-        def __str__(self):
-            return '<%s, %s>' % self
-
     def __init__(self):
-        self.left = set()
-        self.right = set()
-        self.pairs = set()
+        self.left = dict()
+        self.right = dict()
 
     def add(self, left_el, right_el):
-        pair = Bimap.Pair(left_el, right_el)
-        self.pairs.add(pair)
-        self.left.add(pair.left_view())
-        self.right.add(pair.right_view())
+        self.left[left_el] = right_el
+        self.right[right_el] = left_el
 
     def __str__(self):
-        return '{%s}' % ''.join(
-            str(pair) for pair in self.pairs
-        )
+        return '{<%s, %s>}' % ''.join(self.left.iteritems())
 
     def __len__(self):
-        assert len(self.pairs) == len(self.left) == len(self.right)
-        return len(self.pairs)
+        assert len(self.left) == len(self.right)
+        return len(self.left)
 
     def __getitem__(self, index):
-        return self.left[index].right
+        return self.left[index]
 
--- bimap_test.py
+++ bimap_test.py
@@ -10,8 +10,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_add(self):
         self.assertEquals(len(self.bimap), 0)
-        self.bimap.add('foo', 'bar')
+        left = 'foo'
+        right = 'bar'
+        self.bimap.add(left, right)
         self.assertEquals(len(self.bimap), 1)
+        self.assertEquals(self.bimap[left], right)
 
 
 if __name__ == '__main__':
